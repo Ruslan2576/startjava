224a6401f146392c0951a6bcaafbe2d55ea1743b
Ruslan <ruslan2576ruslan@yandex.ru>
2025-01-30 16:31:16 +0300

    implement HW_1

---
+++ Lesson_1/MyFirstApp.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class MyFirstApp{
+   public static void main(String[] args){
+       System.out.println("Написано однажды, работает везде!");
+       Scanner console = new Scanner(System.in, "cp866");
+       System.out.println("Введите своё имя: ");
+       System.out.println(console.nextLine());
+    }
+}
