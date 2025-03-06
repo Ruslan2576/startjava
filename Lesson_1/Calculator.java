97471cda50e1b72d4e3679266489c1940b9aed75
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-06 14:21:00 +0300

    implement HW_6

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = console.nextInt();
+        System.out.print("\nВведите второе число: ");
+        int num2 = console.nextInt();
+        System.out.print("\nВведите арифметическую операцию(+, -, *, /, %, ^): ");
+        String operator = console.next();
+
+        int result = 0;
+        if (operator.equals("+")) {
+            result = num1 + num2;
+        } else if (operator.equals("-")) {
+            result = num1 - num2;
+        } else if (operator.equals("*")) {
+            result = num1 * num2;
+        } else if (operator.equals("/")) {
+            result = num1 / num2;
+        } else if (operator.equals("%")) {
+            result = num1 % num2;
+        } else if (operator.equals("^")) {
+            int tmp = 1;
+            for (int i = 0; i < num2; i++) {
+                tmp *= num1;
+            }
+            result = tmp;
+        }
+        System.out.printf("%d %s %d = %d", num1, operator, num2, result);
+    }
+}
