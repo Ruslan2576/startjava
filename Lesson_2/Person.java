da4524c3fd75ec76ec4a6149e90eaedb90a67d88
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-08 22:41:38 +0300

    implement HW_1

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,28 +1,28 @@
 public class Person {
     
-    private String gender = "male";
-    private String name = "John";
-    private int height = 184;
-    private int weight = 100;
-    private int age = 77;
+    String gender = "male";
+    String name = "John";
+    int height = 184;
+    int weight = 100;
+    int age = 77;
 
-    public void move() {
+    void move() {
         System.out.println("Иду");
     }
 
-    public void sit() {
+    void sit() {
         System.out.println("Сижу");
     }
 
-    public void run() {
+    void run() {
         System.out.println("Бегу");
     }
 
-    public void speak() {
+    void speak() {
         System.out.println("Говорю");
     }
 
-    public void learnJava() {
+    void learnJava() {
         System.out.println("Изучаю Java");
     }
 }
