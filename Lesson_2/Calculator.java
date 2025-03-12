43253dac6c127b23333b818d42047742dce8f54c
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-12 17:41:02 +0300

    implement HW_3

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,37 +1,5 @@
-import java.util.Scanner;
-
 public class Calculator {
-    
-    private Scanner scan;
-    private int num1;
-    private int num2;
-    private char sign;
-
-    public void inputMathExpression() {
-        Calculator c = new Calculator();
-        scan = new Scanner(System.in);
-        System.out.print("\nВведите первое число: ");
-        num1 = scan.nextInt();
-        
-        scan.nextLine();
-        while (sign == 0) {
-            System.out.print("\nВведите знак операции (+, -, *, /, ^, %): ");
-            sign = checkSign(scan.nextLine().charAt(0));
-        }
-
-        if (sign == '/') {
-            while (num2 == 0) {
-                System.out.print("\nВведите второе число: ");
-                num2 = scan.nextInt();
-                checkZero(num2);
-            }
-        } else {
-            System.out.print("\nВведите второе число: ");
-            num2 = scan.nextInt();
-        }
-    }
-
-    public void runCulculate() {
+    public int runCalculate(int num1, int num2, char sign) {
         int result = 0;
         if (sign == '+') {
             result = num1 + num2;
@@ -53,32 +21,10 @@ public class Calculator {
                 result *= num1;
             }
         }
-
-        if (num2 < 0) {
-            System.out.printf("%d %s %d = %f%n", num1, sign, num2, (double) 1 / result);
-        } else {
-            System.out.printf("%d %s %d = %d%n", num1, sign, num2, result);
-        }
-        sign = 0;
-        num1 = 0;
-        num2 = 0;
+        return result;
     }
 
-    public boolean continueCalculation() {
-        scan.nextLine();
-        String choice = null;
-        boolean flag = false;
-        do {
-            System.out.println("Хотите продолжить вычисления? [yes/no]:");
-            choice = scan.nextLine();
-            if (choice.equals("yes")) {
-                flag = true;
-            }
-        } while (!(choice.equals("yes") || choice.equals("no")));
-        return flag;
-    }
-
-    private char checkSign(char sign) {
+    public boolean checkSign(char sign) {
         switch (sign) {
             case '+':
             case '-':
@@ -86,20 +32,10 @@ public class Calculator {
             case '/':
             case '%':
             case '^':
-                this.sign = sign;
-                break;
+                return true;
             default:
                 System.out.printf("Ошибка: операция '%c' не поддерживается%n", sign);
         }
-        return this.sign;
-    }
-
-    private int checkZero(int checkedNum) {
-        if (checkedNum != 0) {
-            num2 = checkedNum;
-        } else {
-            System.out.println("Ошибка: деление на ноль запрещено");
-        }
-        return num2;
+        return false;
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,12 +1,62 @@
+import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator myCalculator = new Calculator();
+        CalculatorTest calcTest = new CalculatorTest();
+        Scanner scan = new Scanner(System.in);
         boolean flag = true;
+        int num1;
+
+        // Основной цикл
         while (flag) {
-            myCalculator.inputMathExpression();
-            myCalculator.runCulculate();
-            flag = myCalculator.continueCalculation();
+            // Получаем первое число
+            System.out.print("\nВведите первое число: ");
+            num1 = scan.nextInt();
+
+            // Получаем и проверяем знак операции
+            scan.nextLine();
+            boolean signFlag = false;
+            char sign = 0;
+            while (!signFlag) {
+                System.out.print("\nВведите знак операции (+, -, *, /, ^, %): ");
+                sign = scan.nextLine().charAt(0);
+                signFlag = myCalculator.checkSign(sign);
+            }
+
+            // Получаем второе число
+            int num2 = 0;
+            do {
+                System.out.print("\nВведите второе число: ");
+                num2 = scan.nextInt();
+            } while (!calcTest.checkZero(num2, sign));
+
+            // Вычисляем выражение и выводим результат
+            int result = myCalculator.runCalculate(num1, num2, sign);
+            if (num2 < 0) {
+                System.out.printf("%d %s %d = %.2f%n", num1, sign, num2, (double) 1 / result);
+            } else {
+                System.out.printf("%d %s %d = %d%n", num1, sign, num2, result);
+            }
+
+            // Продолжаем, или как ?
+            scan.nextLine();
+            String choice = null;
+            do {
+                System.out.println("Хотите продолжить вычисления? [yes/no]:");
+                choice = scan.nextLine();
+                if (choice.equals("no")) {
+                    flag = false;
+                }
+            } while (!(choice.equals("yes") || choice.equals("no")));
         }
+    }
+
+    public boolean checkZero(int checkedNum, char sign) {
+        if (checkedNum == 0 && sign == '/') {
+            System.out.println("Ошибка: деление на ноль запрещено");
+            return false;
+        }
+        return true;
     }
 }
