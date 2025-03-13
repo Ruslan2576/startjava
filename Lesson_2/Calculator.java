f1307559628be41bb576478f58f48969fea5fb6d
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-13 14:01:19 +0300

    implement HW_3

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public int runCalculate(int num1, int num2, char sign) {
+    public int calculate(int num1, int num2, char sign) {
         int result = 0;
         if (sign == '+') {
             result = num1 + num2;
@@ -13,29 +13,11 @@ public class Calculator {
             result = num1 % num2;
         } else if (sign == '^') {
             result = 1;
-            int tmp = num2;
-            if (num2 < 0) {
-                tmp = -num2;
-            }
-            for (int i = 0; i < tmp; i++) {
+            num2 = Math.abs(num2);
+            for (int i = 0; i < num2; i++) {
                 result *= num1;
             }
         }
         return result;
-    }
-
-    public boolean checkSign(char sign) {
-        switch (sign) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '%':
-            case '^':
-                return true;
-            default:
-                System.out.printf("Ошибка: операция '%c' не поддерживается%n", sign);
-        }
-        return false;
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,61 +2,72 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator myCalculator = new Calculator();
-        CalculatorTest calcTest = new CalculatorTest();
+        Calculator calculator = new Calculator();
         Scanner scan = new Scanner(System.in);
-        boolean flag = true;
-        int num1;
+        String choice = "";
 
         // Основной цикл
-        while (flag) {
+        while (!choice.equals("no")) {
             // Получаем первое число
             System.out.print("\nВведите первое число: ");
-            num1 = scan.nextInt();
+            int num1 = scan.nextInt();
 
             // Получаем и проверяем знак операции
             scan.nextLine();
-            boolean signFlag = false;
             char sign = 0;
-            while (!signFlag) {
+            do {
                 System.out.print("\nВведите знак операции (+, -, *, /, ^, %): ");
                 sign = scan.nextLine().charAt(0);
-                signFlag = myCalculator.checkSign(sign);
-            }
+            } while (!checkSign(sign));
 
             // Получаем второе число
             int num2 = 0;
             do {
                 System.out.print("\nВведите второе число: ");
                 num2 = scan.nextInt();
-            } while (!calcTest.checkZero(num2, sign));
+            } while (!checkZero(num2, sign));
 
             // Вычисляем выражение и выводим результат
-            int result = myCalculator.runCalculate(num1, num2, sign);
-            if (num2 < 0) {
-                System.out.printf("%d %s %d = %.2f%n", num1, sign, num2, (double) 1 / result);
-            } else {
-                System.out.printf("%d %s %d = %d%n", num1, sign, num2, result);
-            }
+            int result = calculator.calculate(num1, num2, sign);
+            printResult(num1, num2, sign, result);
 
             // Продолжаем, или как ?
             scan.nextLine();
-            String choice = null;
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
                 choice = scan.nextLine();
-                if (choice.equals("no")) {
-                    flag = false;
-                }
-            } while (!(choice.equals("yes") || choice.equals("no")));
+            } while (!choice.equals("yes") && !choice.equals("no"));
         }
     }
 
-    public boolean checkZero(int checkedNum, char sign) {
+    public static boolean checkZero(int checkedNum, char sign) {
         if (checkedNum == 0 && sign == '/') {
             System.out.println("Ошибка: деление на ноль запрещено");
             return false;
         }
         return true;
+    }
+
+    public static boolean checkSign(char sign) {
+        switch (sign) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+            case '^':
+                return true;
+            default:
+                System.out.printf("Ошибка: операция '%c' не поддерживается%n", sign);
+        }
+        return false;
+    }
+
+    public static void printResult(int num1, int num2, char sign, int result) {
+        if (num2 < 0) {
+            System.out.printf("%d %s %d = %.2f%n", num1, sign, num2, (double) 1 / result);
+        } else {
+            System.out.printf("%d %s %d = %d%n", num1, sign, num2, result);
+        }
     }
 }
