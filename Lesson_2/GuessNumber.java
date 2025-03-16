b278f5bd4839d84da14f643c700aa3fede7effd4
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-16 17:45:57 +0300

    implement HW_5

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GuessNumber {
 
     private Player firstPlayer;
@@ -8,7 +10,43 @@ public class GuessNumber {
         this.secondPlayer = secondPlayer;
     }
 
-    public Player checkNumber(int computerChoice) {
+    public Player inputNumbers() {
+        Player winner = null;
+        Scanner scan = new Scanner(System.in);
+        int computerChoice = (int) (Math.random() * 100) + 1;
+
+        int counter = 1;
+        while (winner == null) {
+            int number = 0;
+            if (counter % 2 != 0) {
+                // Игрок №1 вводит число
+                do {
+                    System.out.print(firstPlayer.getName() + " угадайте число в отрезке [1, 100]: ");
+                    number = scan.nextInt();
+                } while (number < 1 || number > 101);
+                firstPlayer.setNumber(number);
+            } else {
+                // Игрок №2 вводит число
+                do {
+                    System.out.print(secondPlayer.getName() + " угадайте число в отрезке [1, 100]: ");
+                    number = scan.nextInt();
+                } while (number < 1 || number > 101);
+                secondPlayer.setNumber(number);
+            }
+
+            winner = checkNumber(computerChoice);
+            if (winner == null) {
+                System.out.println("неверно");
+            }
+
+            firstPlayer.setNumber(0);
+            secondPlayer.setNumber(0);
+            counter++;
+        }
+        return winner;
+    }
+
+    private Player checkNumber(int computerChoice) {
         if (computerChoice == firstPlayer.getNumber()) {
             return firstPlayer;
         } 
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -15,40 +15,11 @@ public class GuessNumberTest {
         Player secondPlayer = new Player(name);
 
         GuessNumber guessNumber = new GuessNumber(firstPlayer, secondPlayer);
-        Player winner = null;
         String choice = "";
 
-        int computerChoice = (int) (Math.random() * 100) + 1;
         while (!choice.equals("no")) {
-            firstPlayer.setNumber(0);
-            secondPlayer.setNumber(0);
-            do {
-                // Первый игрок вводит число
-                int number = 0;
-                do {
-                    System.out.print(firstPlayer.getName() + " угадайте число в отрезке [1, 100]: ");
-                    number = scan.nextInt();
-                } while (number < 1 || number > 101);
-                firstPlayer.setNumber(number);
-                winner = guessNumber.checkNumber(computerChoice);
-                if (winner != null) {
-                    break;
-                }
-                System.out.println("Неверно");
-
-                // Второй игрок вводит число
-                do {
-                    System.out.print(secondPlayer.getName() + " угадайте число в отрезке [1, 100]: ");
-                    number = scan.nextInt();
-                } while (number < 1 || number > 101);
-                secondPlayer.setNumber(number);
-                winner = guessNumber.checkNumber(computerChoice);
-                if (winner != null) {
-                    break;
-                }
-                System.out.println("Неверно");
-            } while (winner == null);
-            printWinner(winner);
+            // Вывести победителя
+            printWinner(guessNumber.inputNumbers());
 
             // Продолжим?
             do {
@@ -59,6 +30,6 @@ public class GuessNumberTest {
     }
 
     public static void printWinner(Player winner) {
-        System.out.println(winner.getName() + " вы победили");
+        System.out.println(winner.getName() + " Поздравляем вы победили");
     }
 }
