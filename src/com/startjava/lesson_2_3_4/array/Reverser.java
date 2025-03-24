80d29e78c717ad99a4172e74fe025bee48081518
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-24 13:11:09 +0300

    Implement HW_1

--- src/com/startjava/lesson_2_3_4/array/Reverser.java
+++ src/com/startjava/lesson_2_3_4/array/Reverser.java
@@ -5,20 +5,20 @@ import java.util.Arrays;
 public class Reverser {
     public static void main(String[] args) {
         int[] numbers1 = {};
-        int[] numbersAfterReverse = reverse(numbers1);
-        printResult(numbers1, numbersAfterReverse);
+        int[] reversed = reverse(numbers1);
+        printResult(numbers1, reversed);
 
         int[] numbers2 = null;
-        numbersAfterReverse = reverse(numbers2);
-        printResult(numbers2, numbersAfterReverse);
+        reversed = reverse(numbers2);
+        printResult(numbers2, reversed);
 
         int[] numbers3 = {6, 8, 9, 1};
-        numbersAfterReverse = reverse(numbers3);
-        printResult(numbers3, numbersAfterReverse);
+        reversed = reverse(numbers3);
+        printResult(numbers3, reversed);
 
         int[] numbers4 = {13, 8, 5, 3, 2, 1, 1};
-        numbersAfterReverse = reverse(numbers4);
-        printResult(numbers4, numbersAfterReverse);
+        reversed = reverse(numbers4);
+        printResult(numbers4, reversed);
     }
 
     private static int[] reverse(int[] numbers) {
@@ -28,14 +28,13 @@ public class Reverser {
         int[] reversed = new int[numbers.length];
         int counter = numbers.length - 1;
         for (int number : numbers) {
-            reversed[counter] = number;
-            counter--;
+            reversed[counter--] = number;
         }
         return reversed;
     }
 
-    private static void printResult(int[] numbersBeforeReverse, int[] numbersAfterReverse) {
-        System.out.println("   До реверса: " + Arrays.toString(numbersBeforeReverse));
-        System.out.println("После реверса: " + Arrays.toString(numbersAfterReverse));
+    private static void printResult(int[] original, int[] reversed) {
+        System.out.println("   До реверса: " + Arrays.toString(original));
+        System.out.println("После реверса: " + Arrays.toString(reversed));
     }
 }
