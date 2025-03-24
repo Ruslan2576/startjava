345924a1fa135dc4a77491b96d87ec92e88313d0
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-24 19:54:59 +0300

    Implement HW_2

--- src/com/startjava/lesson_2_3_4/array/Factorial.java
+++ src/com/startjava/lesson_2_3_4/array/Factorial.java
@@ -3,61 +3,63 @@ package com.startjava.lesson_2_3_4.array;
 public class Factorial {
     public static void main(String[] args) {
         int[] numbers1 = {};
-        int[] factorial1 = getFactorial(numbers1);
-        printResult(numbers1, factorial1);
+        int[] factorial1 = calc(numbers1);
+        printExpr(numbers1, factorial1);
 
         int[] numbers2 = null;
-        int[] factorial2 = getFactorial(numbers2);
-        printResult(numbers2, factorial2);
+        int[] factorial2 = calc(numbers2);
+        printExpr(numbers2, factorial2);
 
         int[] numbers3 = {8, 0, 9};
-        int[] factorial3 = getFactorial(numbers3);
-        printResult(numbers3, factorial3);
+        int[] factorial3 = calc(numbers3);
+        printExpr(numbers3, factorial3);
 
         int[] numbers4 = {-3, 1, 7, 13};
-        int[] factorial4 = getFactorial(numbers4);
-        printResult(numbers4, factorial4);
+        int[] factorial4 = calc(numbers4);
+        printExpr(numbers4, factorial4);
 
         int[] numbers5 = {-22, -0};
-        int[] factorial5 = getFactorial(numbers5);
-        printResult(numbers5, factorial5);
+        int[] factorial5 = calc(numbers5);
+        printExpr(numbers5, factorial5);
     }
 
-    private static int[] getFactorial(int... numbers) {
+    private static int[] calc(int... numbers) {
         if (numbers == null) {
             return null;
         }
-        int[] factorial = new int[numbers.length];
+        int[] factorials = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < 1) {
-                factorial[i] = numbers[i];
+            if (numbers[i] < 0) {
                 continue;
             }
             int result = 1;
-            for (int j = 1; j <= numbers[i]; j++) {
+            for (int j = 2; j <= numbers[i]; j++) {
                 result *= j;
             }
-            factorial[i] = result;
+            factorials[i] = result;
         }
-        return factorial;
+        return factorials;
     }
 
-    private static void printResult(int[] original, int[] factorial) {
+    private static void printExpr(int[] original, int[] factorials) {
         if (original == null) {
             return;
         }
         for (int i = 0; i < original.length; i++) {
-            if (original[i] < 1) {
+            if (original[i] < 0) {
                 System.out.printf("Ошибка: факториал %d! не определен%n", original[i]);
                 continue;
+            } else if (original[i] <= 1) {
+                System.out.println(original[i] + "! = 1");
+                continue;
             }
-            StringBuilder result = new StringBuilder("!" + original[i] + " = ");
+
+            StringBuilder expr = new StringBuilder(original[i] + "! = ");
             for (int j = 1; j <= original[i]; j++) {
-                result.append(j).append(" * ");
+                expr.append(j == original[i] ? j : j + " * ");
             }
-            result.delete(result.length() - 3, result.length());
-            result.append(original[i] == 1 ? "" : " = " + factorial[i]);
-            System.out.println(result);
+            expr.append(" = ").append(factorials[i]);
+            System.out.println(expr);
         }
         System.out.println();
     }
