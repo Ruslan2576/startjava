555187b5d29541301cf0d18d0838dbe16e615a65
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-26 22:42:40 +0300

    implement HW_3

--- src/com/startjava/lesson_2_3_4/array/Replacer.java
+++ src/com/startjava/lesson_2_3_4/array/Replacer.java
@@ -1,7 +1,5 @@
 package com.startjava.lesson_2_3_4.array;
 
-import java.util.Arrays;
-
 public class Replacer {
     public static void main(String[] args) {
         float[] numbers = new float[15];
@@ -9,22 +7,32 @@ public class Replacer {
             numbers[i] = (float) Math.random();
         }
 
-        printArray(numbers, replace(numbers, -1), -1);
-        printArray(numbers, replace(numbers, 15), 15);
-        printArray(numbers, replace(numbers, 0), 0);
-        printArray(numbers, replace(numbers, 14), 14);
+        replaceNumbers(numbers, -1);
+        replaceNumbers(numbers, 15);
+        replaceNumbers(numbers, 0);
+        replaceNumbers(numbers, 14);
     }
 
-    private static float[] replace(float[] numbers, int index) {
-        if (index + 1 < 0 || index + 1 >= numbers.length) {
-            return numbers;
+    private static void replaceNumbers(float[] numbers, int index) {
+        if (index < 0 || index >= numbers.length) {
+            System.out.println("Ошибка: вы вышли за пределы индекса массива.");
+            return;
         }
-        float[] numbersCopy = Arrays.copyOf(numbers, numbers.length);
-        Arrays.fill(numbersCopy, index + 1, numbers.length, 0);
-        return numbersCopy;
+
+        int count = 0;
+        float[] replacedNumbers = new float[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > numbers[index]) {
+                replacedNumbers[i] = 0;
+                count++;
+                continue;
+            }
+            replacedNumbers[i] = numbers[i];
+        }
+        printArray(numbers, replacedNumbers, index, count);
     }
 
-    private static void printArray(float[] original, float[] replacedNumbers, int index) {
+    private static void printArray(float[] original, float[] replacedNumbers, int index, int count) {
         System.out.println("Исходный массив");
         for (int i = 0; i < original.length; i++) {
             System.out.printf("%.3f ", original[i]);
@@ -32,24 +40,15 @@ public class Replacer {
                 System.out.println();
             }
         }
-        System.out.println("\n");
 
-        System.out.println("Изменённый массив");
+        System.out.println("\nИзменённый массив");
         for (int i = 0; i < replacedNumbers.length; i++) {
             System.out.printf("%.3f ", replacedNumbers[i]);
             if (i == 7) {
                 System.out.println();
             }
         }
-        System.out.println();
-
-        if (index > replacedNumbers.length - 1 || index < 0) {
-            System.out.printf("Ошибка: индекс %d отсутствует%n", index);
-        } else {
-            System.out.printf("Значение из ячейки по индексу %d: %.3f%n", index, replacedNumbers[index]);
-        }
-        int counterZero = index >= replacedNumbers.length ||
-                index < -1 ? 0 : replacedNumbers.length - 1 - index;
-        System.out.printf("Количество обнулённых ячеек: %d%n%n", counterZero);
+        System.out.printf("%nЗначение из ячейки по индексу %d: %.3f%n", index, original[index]);
+        System.out.printf("Количество обнулённых ячеек: %d%n%n", count);
     }
 }
