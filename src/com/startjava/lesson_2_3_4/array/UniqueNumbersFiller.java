34ef02f16085bad4dd111f1bedd7bcc852dda657
Ruslan <ruslan2576ruslan@yandex.ru>
2025-03-31 16:48:43 +0300

    implement HW_5

--- src/com/startjava/lesson_2_3_4/array/UniqueNumbersFiller.java
+++ src/com/startjava/lesson_2_3_4/array/UniqueNumbersFiller.java
@@ -5,14 +5,14 @@ import java.util.Random;
 
 public class UniqueNumbersFiller {
     public static void main(String[] args) {
-        printSortedNumbers(createRandomArray(-30, -10, 23));
-        printSortedNumbers(createRandomArray(10, 50, 10));
-        printSortedNumbers(createRandomArray(-34, -34, 0));
-        printSortedNumbers(createRandomArray(-1, 2, -3));
-        printSortedNumbers(createRandomArray(5, -8, 2));
+        printSortedNumbers(createRandomArray(-30, -10, 23), 23);
+        printSortedNumbers(createRandomArray(10, 50, 10), 10);
+        printSortedNumbers(createRandomArray(-34, -34, 0), 0);
+        printSortedNumbers(createRandomArray(-1, 2, -3), -3);
+        printSortedNumbers(createRandomArray(5, -8, 2), 2);
     }
 
-    public static int[][] createRandomArray(int start, int stop, int count) {
+    public static int[] createRandomArray(int start, int stop, int count) {
         int len = (int) (Math.abs(start - stop) * .75);
         if (count < 1) {
             System.out.printf("Ошибка: количество чисел в строке не должно быть < 1 (%d)%n", count);
@@ -29,39 +29,41 @@ public class UniqueNumbersFiller {
             return null;
         }
 
-        if (count > Math.abs(stop - start)) {
-            count = Math.abs(stop - start + 1);
-        }
-
-        int[][] numbers = new int[len][count];
+        int[] numbers = new int[len];
         Random rand = new Random();
         for (int i = 0; i < len; i++) {
-            for (int j = 0; j < count; j++) {
-                boolean isEquals = false;
-                int randomNumber = rand.nextInt(start, stop + 1);
-                for (int k = 0; k < count; k++) {
-                    if (numbers[i][k] == randomNumber) {
-                        isEquals = true;
-                        j--;
-                        break;
-                    }
+            boolean isEquals = false;
+            int randomNumber = rand.nextInt(start, stop + 1);
+            for (int j = 0; j < i; j++) {
+                if (numbers[j] == randomNumber) {
+                    isEquals = true;
+                    i--;
+                    break;
                 }
-                if (!isEquals) {
-                    numbers[i][j] = randomNumber;
-                }
+            }
+            if (!isEquals) {
+                numbers[i] = randomNumber;
             }
         }
         return numbers;
     }
 
-    public static void printSortedNumbers(int[][] numbers) {
+    public static void printSortedNumbers(int[] numbers, int width) {
         if (numbers == null) {
             return;
         }
-        for (int[] number : numbers) {
-            Arrays.sort(number);
-            System.out.println(Arrays.toString(number));
+
+        Arrays.sort(numbers);
+        int count = 1;
+        for (int number : numbers) {
+            System.out.printf("%3d ", number);
+            if (count == width) {
+                System.out.println();
+                count = 0;
+            }
+            count++;
         }
+        System.out.println("\n");
     }
 }
 
