4021993c2e0d1d7b43b9b53677ca16c6d0768763
Ruslan <ruslan2576ruslan@yandex.ru>
2025-04-23 16:09:52 +0300

    implements HW_1

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,11 +1,9 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.lang.Math;
-import java.text.DecimalFormat;
-import javax.naming.OperationNotSupportedException;
 
 public class Calculator {
-    public static void calculate(String expression) {
+    public static Double calculate(String expression) {
         String[] elements = expression.split(" ");
         double result;
         String sign = null;
@@ -27,28 +25,19 @@ public class Calculator {
                 }
                 case "%" -> Math.floorMod(num1, num2);
                 case "^" -> Math.pow(num1, num2);
-                default -> throw new OperationNotSupportedException();
+                default -> throw new UnsupportedOperationException();
             };
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("Ошибка: неправильный ввод выражения");
-            return;
         } catch (ArithmeticException ex) {
             System.out.println("Ошибка: деление на ноль запрещено");
-            return;
+            return null;
         } catch (NumberFormatException ex) {
             System.out.println("Ошибка: вы использовали не целые числа");
-            return;
-        } catch (OperationNotSupportedException ex) {
+            return null;
+        } catch (UnsupportedOperationException ex) {
             System.out.printf("Ошибка: операция '%s' не поддерживается%n", sign);
             System.out.println("Доступные операции [+, -, *, /, %, ^]");
-            return;
+            return null;
         }
-
-        printResult(expression, result);
-    }
-
-    private static void printResult(String expression, double result) {
-        DecimalFormat df = new DecimalFormat("#.###");
-        System.out.println(expression + " = " + df.format(result));
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,14 +1,15 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String choice = "";
+        String choice = "yes";
 
         while (!choice.equals("no")) {
-            if (!choice.equals("yes") && !choice.isEmpty()) {
+            if (!choice.equals("yes")) {
                 System.out.print("Введите корректный ответ [yes / no]: ");
                 choice = scan.nextLine().toLowerCase();
                 if (!choice.equals("yes")) {
@@ -16,13 +17,27 @@ public class CalculatorTest {
                 }
             }
 
-            System.out.print("Введите выражение из трех аргументов, например, 2 ^ 10: ");
-            String expression = scan.nextLine().replaceAll("\\s+", " ").trim();
-            Calculator.calculate(expression);
+            String expression = inputExpr(scan);
+            Double result = Calculator.calculate(expression);
+
+            // Вывести результат
+            if (result != null) {
+                printResult(expression, result);
+            }
 
             // Продолжаем, или как?
             System.out.print("Хотите продолжить игру [yes / no]: ");
             choice = scan.nextLine().toLowerCase();
         }
+    }
+
+    private static String inputExpr(Scanner scan) {
+        System.out.print("Введите выражение из трех аргументов, например, 2 ^ 10: ");
+        return scan.nextLine().replaceAll("\\s+", " ").trim();
+    }
+
+    private static void printResult(String expression, double result) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        System.out.println(expression + " = " + df.format(result));
     }
 }
