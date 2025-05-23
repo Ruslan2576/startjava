6d34463f07178a888ddf81a365cf7e8c7976b20f
Ruslan <ruslan2576ruslan@yandex.ru>
2025-05-23 19:53:52 +0300

    implements HW_1

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -2,15 +2,15 @@
 \c
 drop table if exists jaegers;
 create table jaegers (
-	id 				serial not null primary key,
-	model_name 		text not null,
- 	mark 			char(6) not null,
- 	height 			numeric not null,
-	weight 			numeric not null,
-	status 			text not null,
- 	origin 			text not null,
-	launch 			date not null,
-	kaiju_kill 		integer not null
+    id              serial not null primary key,
+    model_name      text not null,
+    mark            char(6) not null,
+    height          numeric not null,
+    weight          numeric not null,
+    status          text not null,
+    origin          text not null,
+    launch          date not null,
+    kaiju_kill      integer not null
 );
 
 \i populate.sql
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -25,16 +25,17 @@ select *
 select *
   from jaegers
  where launch = (select min(launch)
-  from jaegers);
+                   from jaegers);
 
 -- вывести информацию из столбцов model_name, mark, launch, kaiju_kill тех роботов, которые уничтожили больше всех kaiju
 select model_name, mark, launch, kaiju_kill
   from jaegers
- where kaiju_kill = (select max(kaiju_kill) from jaegers)
+ where kaiju_kill = (select max(kaiju_kill)
+                       from jaegers)
  order by model_name;
 
 -- вывести средний вес роботов округлённый до трёх знаков после запятой
-select round(avg(weight), 3)
+select round(avg(weight), 3) as avg_weight
   from jaegers;
 
 -- увеличить на единицу kaiju_kill у неразрушенных роботов
@@ -45,6 +46,7 @@ update jaegers
 -- удалить уничтоженных роботов, а затем отобразить оставшихся
 delete from jaegers
  where status = 'Destroyed';
+
 select *
   from jaegers
  order by model_name;
